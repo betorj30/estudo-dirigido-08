bad7dd0d01d3cd56069ac73b2ce209d5d4f25af5
Roberto Baptista <baptista.roberto@gmail.com>
2025-12-01 20:44:06 -0300

    apps criados na branch develop

--- projeto/settings.py
+++ projeto/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'rest_framework',
+    'bens',
+    'unidades',
+    'inventario',
+    'simulador',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
