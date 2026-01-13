d889ac8f04a6ff236169aebf3c2df51310b6b38b
Roberto Baptista <baptista.roberto@gmail.com>
2026-01-13 19:00:55 -0300

    estudo dirigido 9

--- core/models.py
+++ core/models.py
@@ -54,11 +54,11 @@ class Bem(models.Model):
      
 class Categoria(models.Model):
     nome = models.CharField(max_length=100)
-    descricao = models.TextField(blank=True)
+    descricao = models.TextField(blank=True) 
 
     class Meta:
         verbose_name = "Categoria"
-        verbose_name_plural = "Categorias" 
+        verbose_name_plural = "Categorias"
 
     def __str__(self):
         return self.nome
